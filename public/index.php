dffcbac3ebafa6c1607a441bd8d87c7e98ce8bb6
Zazou <aito.kenza@gmail.com>
2024-01-27 17:38:27 +0100

    modification suite echec connecion

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+#require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
+// Comment the following line when deploying to production
+// (see https://symfony.com/doc/current/deployment.html#using-the-dev-environment)
+Debug::enable();
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+   return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+   $request = Request::createFromGlobals();
+    $response = $kernel->handle($request);
+    $response->send();
+    $kernel->terminate($request, $response);
 };
+
+#$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
--- src/Controller/Tp6Controller.php
+++ src/Controller/Tp6Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Habitants;
+use App\Repository\HabitantsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class Tp6Controller extends AbstractController
 {
     #[Route('/tp6', name: 'app_tp6')]
-    public function index(): Response
+
+    public function index(HabitantRepository $habitantRepository): Response
     {
-        return $this->render('tp6/index.html.twig', [
-            'controller_name' => 'Tp6Controller',
+        $habitants = $habitantRepository->findAll();
+
+        return $this->render('habitant/index.html.twig', [
+            'habitants' => $habitants,
         ]);
     }
 }
